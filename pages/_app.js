3e5047a2915c7bbdcbb9d64d8e607a6980891c78
Adegboyega <gboyega.banjo@gmail.com>
2022-06-21 18:36:40 +0100

    google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,15 @@ function MyApp({ Component, pageProps }) {
       {/**Global site tag (gtag.js) - Google Analytics */}
       <Script
         strategy="lazyOnload"
-        src="https://www.googletagmanager.com/gtag/js?id=G-YX0FVV9D1L"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', "G-YX0FVV9D1L", {
+          gtag('config', '${process.env.NEXT_GOOGLE_ANALYTICS}', {
           page_path: window.location.pathname,
           });
         `}
